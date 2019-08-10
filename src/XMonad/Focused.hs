6e2a5cc17b7a685bd5c78c7aa5bd768e4191c1f8
Keith <wygulmage@users.noreply.github.com>
2019-08-10 08:21:21 -0400

    added some isomorphisms.

--- src/XMonad/Focused.hs
+++ src/XMonad/Focused.hs
@@ -3,7 +3,13 @@
   , TypeOperators
   #-}
 
-module XMonad.Focused where
+module XMonad.Focused
+    ( Focused, mkFocused, single
+    , focus
+    , fromNonEmpty
+    , rotateUp, rotated, focusToTop
+    , insert, delete
+    ) where
 
 import Control.Applicative (Applicative ((<*>), pure))
 import Control.Category (Category ((.)))
@@ -15,13 +21,11 @@ import Data.Semigroup
 import Data.Semigroup.Foldable
 import Data.Semigroup.Traversable
 import Data.Traversable (fmapDefault, foldMapDefault)
-import qualified Data.List as List
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NonEmpty
 import Data.Maybe (Maybe (..))
 
 
--- newtype Focused a = Focused ([a], a, [a])
 newtype Focused a = Focused ([a], NonEmpty a)
 
 focus :: Lens' (Focused a) a
@@ -34,7 +38,6 @@ mkFocused ups x dns = Focused (ups, x :| dns)
 ----- Instances -----
 
 instance Functor Focused where
-    -- fmap f (Focused xup x xdn) = Focused (fmap f xup) (f x) (fmap f xdn)
     fmap = fmapDefault
 
 instance Foldable Focused where
@@ -49,13 +52,16 @@ instance Foldable1 Focused where
     toNonEmpty xs = xs'
         where (Focused (_, xs')) = focusToTop xs
 
-
 instance Traversable1 Focused where
     traverse1 f (Focused (up : ups, dn)) = ((.).(.)) Focused (,) <$> fmap toList (traverse1 f (up :| ups)) <.> traverse1 f dn
     traverse1 f (Focused (_, dn)) = fromNonEmpty <$> traverse1 f dn
 
 instance Semigroup (Focused a) where
     Focused (up, dn) <> xs = Focused (up, dn <> toNonEmpty xs)
+
+instance Reversing (Focused a) where
+    reversing (Focused (up, x :| dn)) = mkFocused dn x up
+
 
 ----- Functions -----
 
@@ -65,20 +71,18 @@ fromNonEmpty = Focused . (,) []
 
 rotateUp :: Focused a -> Focused a
 -- ^ Move the focus up, treating the whole Focused structure as a loop. O(n) worst case, O(1) average case.
-rotateUp (Focused (x' : ups, x :| dn)) = Focused (ups, x' :| x : dn)
+rotateUp (Focused (x' : ups, x :| dn)) = mkFocused ups x' (x : dn)
 rotateUp (Focused (_, dn)) =
     case NonEmpty.reverse dn of
-    x :| up -> Focused (up, x :| [])
+    x :| up -> mkFocused up x []
 
+rotated :: Iso' (Focused a) (Focused a)
+rotated = iso rotateUp (under reversed rotateUp)
 
 focusToTop :: Focused a -> Focused a
 -- ^ Shift the focus all the way up (O(n)).
-focusToTop (Focused (x' : up, x :| dn)) = focusToTop (Focused (up, x' :| x : dn))
+focusToTop (Focused (x' : up, x :| dn)) = focusToTop (mkFocused up x' (x : dn))
 focusToTop xs = xs
-
-reverse :: Focused a -> Focused a
-reverse (Focused (up, x :| dn)) = Focused (dn, x :| up)
-
 
 single :: a -> Focused a
 -- ^ Focus on a single item (O(1)).
@@ -86,10 +90,10 @@ single = fromNonEmpty . pure
 
 insert :: a -> Focused a -> Focused a
 -- ^ Insert a new focus above the old (O(1)).
-insert x' (Focused (up, x :| dn)) = Focused (up, x' :| x : dn)
+insert x' (Focused (up, x :| dn)) = mkFocused up x' (x : dn)
 
 delete :: Focused a -> Maybe (Focused a)
 -- ^ Replace the focus with the item below it, if possible, or the item above it, or nothing (O(1)).
-delete (Focused (up, _ :| x : dn)) = Just (Focused (up, x :| dn))
-delete (Focused (x : up, _)) = Just (Focused (up, pure x))
+delete (Focused (up, _ :| x : dn)) = Just (mkFocused up x dn)
+delete (Focused (x : up, _)) = Just (mkFocused up x [])
 delete _ = Nothing
