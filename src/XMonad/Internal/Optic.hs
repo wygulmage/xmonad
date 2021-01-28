36fb16226d003b02ac4e81a8e35988f74f0fb242
Keith <wygulmage@users.noreply.github.com>
2021-01-27 21:07:25 -0500

    deleted 'views'.
    It's not needed.

--- src/XMonad/Internal/Optic.hs
+++ src/XMonad/Internal/Optic.hs
@@ -11,10 +11,6 @@ import Lens.Micro.Internal (foldrOf, ( #. ))
 
 type SimpleOptic f ta a = (a -> f a) -> ta -> f ta
 
-views :: MonadReader s m => SimpleOptic (Const b) s a -> (a -> b) -> m b
--- In general, `views o f` = `f <$> view o f`, but unlike `view`, `views` can `foldMap` a Reader Monad.
-views o f = asks (getConst #. o (Const #. f))
-
 --- Is it better to provide Traversal variants of Foldable methods, or to use the Foldable methods with toListOf? Lists are a bit more versatile, since you can `filter p . toListOf` but there's no 'filterOf'.
 findOf :: Alternative m => Getting (Endo (m a)) ta a -> (a -> Bool) -> ta -> m a
 findOf o p =
