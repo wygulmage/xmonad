d0d381a0037b11d140dbd6ced416e812b13aaadc
Keith <wygulmage@users.noreply.github.com>
2021-03-12 00:29:31 -0500

    removed unused import from ExtensibleState.

--- src/XMonad/ExtensibleState.hs
+++ src/XMonad/ExtensibleState.hs
@@ -31,7 +31,6 @@ import qualified Control.Monad.State as State
 
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe)
 import Data.Typeable (cast, typeOf)
 
 import Text.Read (readMaybe)
