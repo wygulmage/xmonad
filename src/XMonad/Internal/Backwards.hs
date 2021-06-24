9a77de47ef9544231960a8bd461e4de78920f1a9
wygulmage <wygulmage@users.noreply.github.com>
2021-06-24 15:56:22 -0400

    added INLINE pragmas to internal Backwards module.

--- src/XMonad/Internal/Backwards.hs
+++ src/XMonad/Internal/Backwards.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module XMonad.Internal.Backwards where
--- | This module defines a 'backwards' applicative using 'liftA2'. Remove it when transformers's 'Control.Applicative.Backwards' uses 'liftA2'.
+-- | This module defines a 'backwards' applicative using 'liftA2'. Remove it when transformers's 'Control.Applicative.Backwards' uses 'liftA2'. It only defines the instances necessary for its internal use.
 
 
 import Control.Applicative
@@ -15,5 +15,8 @@ forwards (Backwards mx) = mx
 
 instance (Applicative m)=> Applicative (Backwards m) where
    pure x = Backwards (pure x)
-   liftA2 f (Backwards mx) (Backwards my) = Backwards $ liftA2 (flip f) my mx
+   {-# INLINE pure #-}
+   liftA2 f (Backwards mx) (Backwards my) = Backwards (liftA2 (flip f) my mx)
+   {-# INLINE liftA2 #-}
    (*>) = liftA2 (\ _ x -> x) -- You have to use 'liftA2' either way.
+   {-# INLINE (*>) #-}
