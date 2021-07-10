b37c9715fc43c6c4846d26088c10599b8ff76a52
Keith <wygulmage@users.noreply.github.com>
2021-07-10 18:23:42 -0400

    silenced warnings in Full layout tests

--- tests/Properties/Layout/Full.hs
+++ tests/Properties/Layout/Full.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-} -- Silence warnings about using pureLayout and pureMessage when testing them.
 module Properties.Layout.Full where
 
 import Test.QuickCheck
