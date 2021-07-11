fcd535ed12e96a98de58605ace10a328bcd52148
Keith <wygulmage@users.noreply.github.com>
2021-07-11 14:42:41 -0400

    refactored Layout
    + added an Ord instance for `CLR` that considers `CL` less than `CR`
    * changed some `case x of Just _ -> ...` expressions to `if null x then ...`
    * reordered some `flip f y =<< mx` expressions to `mx >>= \ x -> f x y` (seems
    less noisy to me, especially next to the unflipped version, but YMMV)
    * made some other minor stylistic changes
    
    reformatted `splitVertically`
    
    Hopefully it should be more clear / less noisy now.
    
    further clarified and simplified `splitVertically`

--- src/XMonad/Layout.hs
+++ src/XMonad/Layout.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, PatternGuards #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, PatternGuards, ScopedTypeVariables, RankNTypes #-}
 
 -- --------------------------------------------------------------------------
 -- |
@@ -29,7 +29,7 @@ import XMonad.Core
 import Graphics.X11 (Dimension, Position, Rectangle(..))
 import qualified XMonad.StackSet as W
 import Control.Arrow ((***), second)
-import Control.Applicative (liftA2)
+import Control.Applicative ((<|>), liftA2)
 import Control.Monad
 import Data.Foldable (toList)
 import qualified Data.List.NonEmpty as NonEmpty
@@ -38,12 +38,13 @@ import Data.Maybe (fromMaybe)
 ------------------------------------------------------------------------
 
 -- | Change the size of the master pane.
-data Resize     = Shrink | Expand
+data Resize = Shrink | Expand
+
+instance Message Resize
 
 -- | Increase the number of clients in the master pane.
 newtype IncMasterN = IncMasterN Int
 
-instance Message Resize
 instance Message IncMasterN
 
 -- | Simple fullscreen mode. Renders the focused window fullscreen.
@@ -61,20 +62,18 @@ data Tall a = Tall
   deriving (Show, Read)
 
 
--- a nice pure layout, lots of properties for the layout, and its messages, in Properties.hs
+-- a nice pure layout instance, lots of properties for the layout, and its messages, in Properties.hs
 instance LayoutClass Tall a where
     pureLayout (Tall nmaster _ frac) r s = zip ws rs
       where
-      ws = W.integrate s
-      rs = tile frac r nmaster (length ws)
+        ws = W.integrate s
+        rs = tile frac r nmaster (length ws)
 
     pureMessage (Tall nmaster delta frac) m =
-            msum [fmap resize     (fromMessage m)
-                 ,fmap incmastern (fromMessage m)]
-
+        resize <$> fromMessage m  <|>  incmastern <$> fromMessage m
       where
-        resize Shrink             = Tall nmaster delta (max 0 $ frac-delta)
-        resize Expand             = Tall nmaster delta (min 1 $ frac+delta)
+        resize Shrink = Tall nmaster delta (max 0 $ frac-delta)
+        resize Expand = Tall nmaster delta (min 1 $ frac+delta)
         incmastern (IncMasterN d) = Tall (max 0 (nmaster+d)) delta frac
 
     description _ = "Tall"
@@ -102,21 +101,30 @@ tile frac r nmaster n
 -- Divide the screen vertically into n subrectangles
 --
 splitVertically, splitHorizontally :: Int -> Rectangle -> [Rectangle]
+{- ^
+For 'splitVertically', each subsequent rectangle will be lower than the previous.
+'splitVertically' and 'splitHorizontally' never evaluate to an empty list; if you tell one to split into zero or fewer parts, it returns the original rectangle.
+-}
+  -- Remember that x and y are the upper left corner of the rectangle, and the coordinates increase as you go down and to the right. So the highest rectangle will have the smallest y value (approximately Rectangle x0 y0 w0 (h0/n)), while the following, lower, rectangles will have higher y values (to approximately y0 + (n - 1) * (h0/n)).
 
 splitVertically n0 r0 = toList $ NonEmpty.unfoldr st (n0, r0)
   where
-    st (n, r@(Rectangle sx sy sw sh))
-      | n <= 1
+    st (i, r@(Rectangle x y w h))
+      | i <= 1
       = (r, Nothing)
       | otherwise
-      = (Rectangle sx sy sw smallh, Just (n - 1, Rectangle sx (sy + dimensionToPosition smallh) sw (sh - smallh)))
+      = (r', Just ( i - 1 , r''))
       where
-        smallh :: Dimension
-        smallh = sh `div` intToDimension n
-        intToDimension :: Int -> Dimension
-        intToDimension = fromIntegral
-        dimensionToPosition :: Dimension -> Position
-        dimensionToPosition = fromIntegral
+        r' = Rectangle x y w h'
+        r'' = Rectangle x (y + toPosition h') w (h - h')
+        h' :: Dimension
+        h' = h `div` intToDimension i
+        -- If you do the logical thing and convert n0 to a Dimension once rather than converting i to a Dimension each iteration, the tests will hang indefinately. I'm not sure why this is.
+        -- Each iteration recalculates height' to be height - height/i, where i is n - the number of iterations performed. So for the first iteration, h' = h0 - h0/n.
+    intToDimension :: Int -> Dimension
+    intToDimension = fromIntegral
+    toPosition :: Dimension -> Position
+    toPosition = fromIntegral
 
 -- Not used in the core, but exported
 splitHorizontally n = fmap mirrorRect . splitVertically n . mirrorRect
@@ -138,8 +146,10 @@ splitVerticallyBy f = (mirrorRect *** mirrorRect) . splitHorizontallyBy f . mirr
 newtype Mirror l a = Mirror (l a) deriving (Show, Read)
 
 instance LayoutClass l a => LayoutClass (Mirror l) a where
-    runLayout (W.Workspace i (Mirror l) ms) r = (fmap (second mirrorRect) *** fmap Mirror)
-                                                `fmap` runLayout (W.Workspace i l ms) (mirrorRect r)
+    runLayout (W.Workspace i (Mirror l) ms) =
+        fmap (fmap (second mirrorRect) *** fmap Mirror)
+        . runLayout (W.Workspace i l ms)
+        . mirrorRect
     handleMessage (Mirror l) = fmap (fmap Mirror) . handleMessage l
     description (Mirror l) = "Mirror " <> description l
 
@@ -195,33 +205,46 @@ infixr 5 |||
 data Choose l r a = Choose !CLR (l a) (r a) deriving (Read, Show) -- TODO Should the (l a) and (r a) parameters be strict too?
 
 -- | Choose the current sub-layout (left or right) in 'Choose'.
-data CLR = CL | CR deriving (Read, Show, Eq)
+data CLR = CL | CR deriving (Read, Show, Eq, Ord)
 
 data NextNoWrap = NextNoWrap deriving (Eq, Show)
 instance Message NextNoWrap
+
+
+-- | A smart constructor that takes some potential modifications, returns a
+-- new structure if any fields have changed, and performs any necessary cleanup
+-- on newly non-visible layouts.
+choose ::
+    (LayoutClass l w, LayoutClass r w)=>
+    Choose l r w -> -- current layout
+    CLR -> -- choice of "left" or "right" layout
+    Maybe (l w) -> -- new "left" layout
+    Maybe (r w) -> -- new "right" layout
+    X (Maybe (Choose l r w))
+choose (Choose d l r) d' ml mr
+  | Nothing <- ml, Nothing <- mr, d == d'
+  = pure Nothing
+  | otherwise
+  = Just <$> makeChoice (fromMaybe l ml) (fromMaybe r mr)
+    where
+      makeChoice l' r' = case compare d d' of
+        LT -> chooseLeft l' r'
+        GT -> chooseRight l' r'
+        EQ -> pure $ Choose d l' r'
+
+      chooseLeft l' r'  = (l' `chooseCL`) <$> hide r'
+      chooseRight l' r' = (`chooseCR` r') <$> hide l'
+
+      chooseCL = Choose CL
+      chooseCR = Choose CR
+
+      hide l' = fromMaybe l' <$> handle l' Hide
 
 -- | A small wrapper around handleMessage, as it is tedious to write
 -- SomeMessage repeatedly.
 handle :: (LayoutClass l a, Message m) => l a -> m -> X (Maybe (l a))
 handle l m = handleMessage l (SomeMessage m)
 
--- | A smart constructor that takes some potential modifications, returns a
--- new structure if any fields have changed, and performs any necessary cleanup
--- on newly non-visible layouts.
-choose ::
-    (LayoutClass l a, LayoutClass r a)=>
-    Choose l r a -> CLR -> Maybe (l a) -> Maybe (r a) ->
-    X (Maybe (Choose l r a))
-choose (Choose d _ _) d' Nothing Nothing | d == d' = pure Nothing
-choose (Choose d l r) d' ml      mr = f lr
-  where
-    (l', r') = (fromMaybe l ml, fromMaybe r mr)
-    lr       = case (d, d') of
-                   (CL, CR) -> (hide l', pure r')
-                   (CR, CL) -> (pure l', hide r')
-                   (_ , _ ) -> (pure l', pure r')
-    f (x,y)  = Just <$> liftA2 (Choose d') x y
-    hide x   = fromMaybe x <$> handle x Hide
 
 instance (LayoutClass l a, LayoutClass r a) => LayoutClass (Choose l r) a where
     runLayout (W.Workspace i (Choose CL l r) ms) =
@@ -232,39 +255,38 @@ instance (LayoutClass l a, LayoutClass r a) => LayoutClass (Choose l r) a where
     description (Choose CL l _) = description l
     description (Choose CR _ r) = description r
 
-    handleMessage lr m | Just NextLayout <- fromMessage m = do
-        mlr' <- handle lr NextNoWrap
-        maybe (handle lr FirstLayout) (pure . Just) mlr'
-
     handleMessage c@(Choose d l r) m | Just NextNoWrap <- fromMessage m =
         case d of
             CL -> do
                 ml <- handle l NextNoWrap
-                case ml of
-                    Just _  -> choose c CL ml Nothing
-                    Nothing -> choose c CR Nothing =<< handle r FirstLayout
+                if null ml
+                  then choose c CR Nothing =<< handle r FirstLayout
+                  else choose c CL ml Nothing
 
             CR -> choose c CR Nothing =<< handle r NextNoWrap
 
     handleMessage c@(Choose _ l _) m | Just FirstLayout <- fromMessage m =
-        flip (choose c CL) Nothing =<< handle l FirstLayout
+        handleMessage l m >>= \ ml -> choose c CL ml Nothing
+
+    handleMessage c m | Just NextLayout <- fromMessage m = do
+        mlr <- handle c NextNoWrap
+        if null mlr
+          then handle c FirstLayout
+          else pure mlr
 
     handleMessage c@(Choose d l r) m | Just ReleaseResources <- fromMessage m =
-        join $ liftA2 (choose c d) (handle l ReleaseResources) (handle r ReleaseResources)
+        join $ liftA2 (choose c d) (handleMessage l m) (handleMessage r m)
 
-    handleMessage c@(Choose d l r) m | Just (JumpToLayout desc) <- fromMessage m = do
-        ml <- handleMessage l m
-        mr <- handleMessage r m
-        let md | desc == description (fromMaybe l ml) = CL
-               | desc == description (fromMaybe r mr) = CR
-               | otherwise = d
-        choose c md ml mr
+    handleMessage c@(Choose _ l r) m | Just (JumpToLayout desc) <- fromMessage m =
+        let
+          jump ml mr
+            | desc == description (fromMaybe l ml) = choose c CL ml Nothing
+            | desc == description (fromMaybe r mr) = choose c CR Nothing mr
+            | otherwise                            = pure Nothing
+        in
+          join $ liftA2 jump (handleMessage l m) (handleMessage r m)
 
     handleMessage c@(Choose d l r) m = do
-        ml' <- case d of
-                CL -> handleMessage l m
-                CR -> pure Nothing
-        mr' <- case d of
-                CL -> pure Nothing
-                CR -> handleMessage r m
-        choose c d ml' mr'
+        case d of
+            CL -> handleMessage l m >>= \ ml -> choose c d ml Nothing
+            CR -> handleMessage r m >>= \ mr -> choose c d Nothing mr
